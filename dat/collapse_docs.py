873b3d027aed6afa5836ad1e647e546763f92d06
ajbc <ajbchaney@gmail.com>
2018-02-22 11:11:25 -0500

    streamlining processing of data scripts; this will temporarily make them out of sync with everything else (to be resolved shortly)

---
+++ dat/collapse_docs.py
@@ -0,0 +1,12 @@
+import sys, os
+
+titles = [title.strip() for title in open(sys.argv[1]).readlines()]
+
+doc_dir = sys.argv[2]
+
+docs = open(doc_dir.strip('/') + '_all.dat', 'w+')
+for title in titles:
+    doc = open(os.path.join(doc_dir, title)).read()
+    doc = doc.replace('\n', ' ')
+    docs.write(doc + '\n')
+docs.close()
