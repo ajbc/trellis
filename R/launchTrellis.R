7b17e9f57e15c4161dc09d7ba268b03c246657e5
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-09-20 11:52:55 -0400

    Revert to suggestions for server packages, in accordance with

--- R/launchTrellis.R
+++ R/launchTrellis.R
@@ -1,20 +1,11 @@
 #' Launch the Trellis shiny app
 #'
-#' TODO(tfs): documentation with in R doc
+#' TODO(tfs): documentation in R doc
 #'
 #' @aliases launchTrellis
 #' @examples
 #' \dontrun{launchTrellis()}
 #' @export
 launchTrellis <- function() {
-	# Load packages required for server to run properly
-	library(shinyjs)
-	library(jsonlite)
-	library(data.table)
-	library(xtable) # Used to sanitize output
-	library(Matrix) # Used for sparse beta
-	library(irlba)  # Used for fast SVD
-	library(rsvd)
-
 	shiny::runApp(system.file('trellis_app/shiny', package='trellis'))
 }
