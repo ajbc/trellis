fca32326deafc025929db96ada475a2c5b5a3f2b
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-09-20 11:39:34 -0400

    Now explicitly load/depend on server dependencies. This seems more semantically correct to me

--- R/launchTrellis.R
+++ R/launchTrellis.R
@@ -7,5 +7,14 @@
 #' \dontrun{launchTrellis()}
 #' @export
 launchTrellis <- function() {
+	# Load packages required for server to run properly
+	library(shinyjs)
+	library(jsonlite)
+	library(data.table)
+	library(xtable) # Used to sanitize output
+	library(Matrix) # Used for sparse beta
+	library(irlba)  # Used for fast SVD
+	library(rsvd)
+
 	shiny::runApp(system.file('trellis_app/shiny', package='trellis'))
 }
