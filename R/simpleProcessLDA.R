ad523d2a2d4ce317fd3aedf09d5d3ee5c0446441
Thomas Schaffner <t.f.schaffner@gmail.com>
2018-09-21 15:51:08 -0400

    Add basic functionality for LDAvis support

--- R/simpleProcessLDA.R
+++ R/simpleProcessLDA.R
@@ -4,40 +4,42 @@
 #' 
 #' @aliases simpleProcessLDA
 #' @param corpus.path Path to directory containing separate text files.
-#' @param out.path Path to output location for the Trellis model
 #' @param k Number of topics to train
-#' @seealso \pkg{\link{trellis.data}} TODO(tfs): Make this a package, or something?
+#' @param out.path Path to output location for the Trellis model. If NULL, ignored.
+#' @param ldavis.data.path Path to output location for term.frequency and doc.length. If NULL, ignored. See \code{trellis::toLDAvis}.
 #' @seealso \pkg{\link{tm}}
 #' @seealso \pkg{\link{topicmodels}}
+#' @seealso \link[dest=https://ldavis.cpsievert.me/reviews/reviews.html]{ldavis}
 #' @examples
-#' # TBD
+#' \dontrun{# TODO}
 #' @export
-simpleProcessLDA <- function(corpus.path, out.path, k) {
+simpleProcessLDA <- function(corpus.path, k, out.path = NULL, ldavis.data.path = NULL) {
 	# TODO(tfs): Add option for saving extra ldavis data
 
 	# Check for appropriate packages
-	tmcheck <- requireNamespace("tm", quiet = TRUE)
-	topicmodelscheck <- requireNamespace("topicmodels", quiet = TRUE)
-	if ((!tmcheck) || (!topicmodelscheck)) {
-		stop("Libraries 'tm' and 'topicmodels' are both required for this processing script.")
+	tmcheck <- requireNamespace("tm")
+	topicmodelscheck <- requireNamespace("topicmodels")
+	slamcheck <- requireNamespace("slam")
+	if ((!tmcheck) || (!topicmodelscheck) || (!slamcheck)) {
+		stop("Libraries 'tm', 'slam', and 'topicmodels' are all required for this processing script.")
 	}
 
 	# Parse files
 	filenames <- list.files(corpus.path)
 	files <- lapply(filenames, function(x) { readLines(file.path(corpus.path, x)) })
-	docs <- Corpus(VectorSource(files))
+	docs <- tm::Corpus(tm::VectorSource(files))
 
-	docs <- tm_map(docs, removePunctuation)
-	docs <- tm_map(docs, content_transformer(tolower))
-	docs <- tm_map(docs, removeNumbers)
-	docs <- tm_map(docs, removeWords, stopwords("english"))
-	docs <- tm_map(docs, stripWhitespace)
-	docs <- tm_map(docs,stemDocument)
-	dtm <- DocumentTermMatrix(docs)
+	docs <- tm::tm_map(docs, tm::removePunctuation)
+	docs <- tm::tm_map(docs, tm::content_transformer(tolower))
+	docs <- tm::tm_map(docs, tm::removeNumbers)
+	docs <- tm::tm_map(docs, tm::removeWords, tm::stopwords("english"))
+	docs <- tm::tm_map(docs, tm::stripWhitespace)
+	docs <- tm::tm_map(docs, tm::stemDocument)
+	dtm <- tm::DocumentTermMatrix(docs)
 	rownames(dtm) <- filenames
 
 	# Run LDA
-	lda.model <- LDA(dtm, k)
+	lda.model <- topicmodels::LDA(dtm, k)
 
 	# Format for Trellis
 	beta <- exp(lda.model@beta)
@@ -46,5 +48,18 @@ simpleProcessLDA <- function(corpus.path, out.path, k) {
 	filenames <- rownames(dtm)
 	titles <- lapply(filenames, function (x) URLdecode(gsub("_", " ", x)))
 
-	save(beta, theta, vocab, titles, filenames, file=out.path)
+	if (!is.null(out.path)) {
+		save(beta, theta, vocab, titles, filenames, file=out.path)
+	}
+
+	if (!is.null(ldavis.data.path)) {
+		term.frequency <- col_sums(dtm)
+		doc.length <- c()
+
+		for (i in seq(nrow(dtm))) {
+			doc.length <- append(doc.length, sum(dtm[i,]))
+		}
+		
+		save(term.frequency, doc.length, file=ldavis.data.path)
+	}
 }
--- R/toLDAvis.R
+++ R/toLDAvis.R
@@ -3,25 +3,33 @@
 #' TODO(tfs): documentation in R doc
 #'
 #' @aliases toLDAvis
-#' @param model Trellis model, formatted as list. Must include beta, theta, vocab
+#' @param model.file .RData file containing Trellis model. Must include beta, theta, vocab. Leaves treated as flat model.
 #' @param doc.length Number of tokens per document, as a vector.
 #' @param term.frequency Term frequencies. Format TBD
 #' @param launch Logical flag. If FALSE, toLDAvis returns the JSON created. Else, launches LDAvis
 #' @seealso \pkg{\link{LDAvis}}
+#' @seealso \pkg{\link{servr}}
 #' @examples
 #' # TBD (how to deal with example data files?)
 #' @export
-toLDAvis <- function(model, doc.length, term.frequency, launch = FALSE) {
-	if (! requireNamespace("LDAvis", quietly = TRUE)) {
+toLDAvis <- function(model.file, doc.length, term.frequency, launch = FALSE) {
+	if (! requireNamespace("LDAvis")) {
 		stop("Please install the LDAvis package: install.packages('LDAvis')")
 	} else {
-		json <- LDAvis::createJSON(phi = model$beta,
-								  theta = model$theta,
-								  doc.length = doc.length,
-								  vocab = model$vocab,
-								  term.frequency = term.frequency)
+		load(model.file)
+		json <- LDAvis::createJSON(phi = beta,
+								   theta = theta,
+								   doc.length = doc.length,
+								   vocab = vocab,
+								   term.frequency = term.frequency)
 
 		if (launch) {
+			servrcheck <- requireNamespace("servr")
+
+			if (!servrcheck) {
+				stop("Please install the servr package to launch LDAvis: install.packages('servr')")
+			}
+
 			LDAvis::serVis(json, out.dir = 'ldavis', open.browser = TRUE)
 		}
 	}
